917184d5f8a509947b3e25c1f86a16f33588cd16
rczerwiec <czerwiecr1999@gmail.com>
2025-03-10 12:05:01 +0100

    [FIX] infobox fix

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -6,11 +6,11 @@ const InfoBox = () => (
     <div className="bg-gray-800 p-6 rounded-lg shadow-md">
       <h3 className="text-yellow-400 font-bold text-xl pb-2">To już dziś! </h3>
       <p>
-        Dziękujemy za ogromne zainteresowanie serwerem w pierwszych 24h! Zakładaliśmy, że na starcie będzie około 30 graczy, jednak serwer w peaku osiągnął liczbę 47 graczy, a łacznie odwiedziło go prawie 200 graczy! Cieszymy się, że serwer wam się podoba i będziemy stale chcieli dodawać wam nowe tryby, funkcje oraz ulepszać to co obecnie mamy.
+        Dziękujemy za ogromne zainteresowanie serwerem w pierwszych 24h! Zakładaliśmy, że na starcie będzie około 30 graczy, jednak serwer w peaku osiągnął liczbę 47 graczy, a łącznie odwiedziło go prawie 200 graczy! Cieszymy się, że serwer wam się podoba i będziemy stale chcieli dodawać wam nowe tryby, funkcje oraz ulepszać to co obecnie mamy.
       </p>
       <br />
       <p>
-        Wiemy również, że serwer boryka się wciąż z wieloma błędami, które na bieżąco łatamy... Pamiętajcie, że serwer jest budowany kompletnie od zera przez zaledwie 5 osób, które robią to wszystko hobbistycznie. Na pierwsze edycje trybów również trzeba zawsze brać poprawkę, że są one swoistymi wersjami "beta".
+        Wiemy również, że serwer boryka się wciąż z wieloma błędami, które na bieżąco łatamy... Pamiętajcie, że serwer jest budowany kompletnie od zera przez zaledwie 5 osób, które robią to wszystko hobbistycznie. Na pierwsze edycje trybów również trzeba zawsze brać poprawkę, że są one swoistymi wersjami &quot;beta&quot;.
       </p>
       <br />
       <p>
